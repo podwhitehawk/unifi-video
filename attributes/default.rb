95152ae2fc4e40f06c0b4ef644f21622778d8f0d
SiruS <podwhitehawk@users.noreply.github.com>
2015-07-10 23:17:41 +0300

    updating to support unifi-video 3.1.1

--- attributes/default.rb
+++ attributes/default.rb
@@ -1,7 +1,7 @@
 default['unifi-video']['package'] = 'unifi-video'
 default['unifi-video']['service'] = 'unifi-video'
 default['unifi-video']['install_method'] = 'package' # possible values 'repo', 'package'
-default['unifi-video']['version'] = '3.0.8'
+default['unifi-video']['version'] = '3.1.1'
 if platform?('debian')
   default['unifi-video']['url'] = "http://dl.ubnt.com/firmwares/unifi-video/#{node['unifi-video']['version']}/unifi-video_#{node['unifi-video']['version']}~Debian7_amd64.deb"
 elsif platform?('ubuntu')
